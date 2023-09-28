428611e31ebb328baf044bdbf1d67839c1aaba21
Carole Belliardo <belliardo.carole@hotmail.fr>
2023-09-28 18:06:43 +0200

    extract_h5

--- curve_rarf/extract_DSK-stdout_info.py
+++ curve_rarf/extract_DSK-stdout_info.py
@@ -1,9 +1,9 @@
 """Usage:
-    extract_DSK-stdout_info.py <nom_rep_input> <output.txt>
+    extract_DSK-stdout_info.py <nom_rep_input> <output_csv>
 
 Arguments:
     nom_rep_input     Le nom du répertoire à traiter
-    output.txt  Le nom du fichier de sortie
+    output_csv  Le nom du fichier de sortie
 
 Options:
     -h --help  Afficher ce message d'aide
@@ -17,85 +17,84 @@ import os
 
 # liste file
 
-
+# liste les fichiers .txt dans un repertoire
 def list_text_files(repertoire):
     fichiers_texte = []
-    for dossier, sous_dossiers, fichiers in os.walk(repertoire):
-        for fichier in fichiers:
-            if fichier.endswith('.txt'):
-                fichiers_texte.append(os.path.join(dossier, fichier))
+    for dossier, sous_dossiers, fichiers in os.walk(repertoire): # parcours de l'arborescence
+        for fichier in fichiers: # parcours des fichiers
+            if fichier.endswith('.txt'): # si le fichier est un fichier texte
+                fichiers_texte.append(os.path.join(dossier, fichier)) # on l'ajoute à la liste
     return fichiers_texte
 
 
 # Fonction pour extraire les informations d'un fichier txt
-def extract_info_from_txt(file_path):
-    info = {}
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+# chaque ligne est stocké dans un dictionnaire 
+# les clés sont les noms des colonnes et les valeurs sont les valeurs de chaque ligne
 
-    current_section = None
-    for line in lines:
-        line = line.strip()
-        if line:
-            if ':' in line:
-                key, value = line.split(':', 1)
-                key = key.strip()
-                value = value.strip()
-                if key == 'bank_uri':
-                    value = parse_filename(value)
-                info[key] = value
-            else:
-                current_section = line.strip()
+def extract_info_from_txt(file_paths, keys_to_extract):
+    info = []
+    for file_path in file_paths:
+        with open(file_path, 'r') as file:
+            info_line = {}
+            for line in file:
+                line = line.strip()              
+                if line:
+                    if ':' in line:
+                        key, value = line.split(':', 1)
+                        key = key.strip()
+                        value = value.strip()
+                        if key in keys_to_extract:
+                            if key == 'bank_uri':
+                                value = parse_filename(value)
+                            info_line[key] = value
+            info.append(info_line)
     return info
 
 
-def extract_info_from_txt_list(txt_liste):
-    dico_result = {}
-    for txt in txt_liste:
-        dico_info = extract_info_from_txt(txt)
-        dico_result.update(dico_info)
-        print('______________________________')
-        print(len(dico_result), len(dico_info))
-    return dico_result
-
-
 def parse_filename(input_string):
-    # Séparer le chemin en répertoires et le nom de fichier
-    chemin, nom_fichier = os.path.split(input_string)
-
-    # Extraire les parties du nom de fichier
-    parties = nom_fichier.split('/')
+    # Extraire les parties du chemin
+    parties = input_string.split('/')
 
     # Initialiser les éléments du nouveau nom de fichier
     nouveau_nom_parts = []
 
     # Parcourir les parties pour extraire les informations requises
+    tech=''
+    subsampling=''
+    
     for partie in parties:
-        if 'illumina' in partie:
-            nouveau_nom_parts.append(partie)
-        elif 'hifi' in partie:
-            nouveau_nom_parts.append(partie)
-        elif partie.startswith('subsampling'):
-            subsampling_parts = partie.split('_')
-            if len(subsampling_parts) >= 2:
-                nouveau_nom_parts.append(subsampling_parts[0])
-                version_part = subsampling_parts[1].split(
-                    '.')[0]  # Retirer l'extension le cas échéant
-                nouveau_nom_parts.append(version_part)
-
+        tech=''
+        print(partie)
+        if (tech == '') and (('illumina' in partie) or ('hifi' in partie)):
+            parties = partie.split('_')
+            print(parties)
+            tech = parties[0]
+            #sampsize = parties[2].split('.')[0]
+            
+        elif 'subsampling' in partie:
+            subsampling = partie.split('_')[0]
     # Construire le nouveau nom de fichier en joignant les parties nécessaires
-    nouveau_nom = '_'.join(nouveau_nom_parts)
+    #nouveau_nom = tech+'_'+subsampling+'_'+sampsize
+    
+    #return nouveau_nom
 
-    # Retirer les extensions ".fasta" et ".fastq" en fin de fichier s'ils existent
-    nouveau_nom = nouveau_nom.replace('.fasta', '').replace('.fastq', '')
 
-    return nouveau_nom
-
+#fonction qui écrit un liste de dict dans un fichier csv
+# les clés sont les noms des colonnes et les valeurs sont les valeurs de chaque lignes
+# chaque élément de la liste est un ligne du fichier csv
+# la première ligne et la liste de clés
+def parse_listOFdico_to_csv(list_dict, output):
+    with open(output, 'w', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=list_dict[0].keys(), delimiter='\t')
+        writer.writeheader()
+        for dico in list_dict:
+            writer.writerow(dico)
+    
 
 def parse_dico_toCSV(dico_info, keys_to_extract, output_file):
 
     if all(key in dico_info for key in keys_to_extract):
-        with open(output_file, 'w', newline='') as csv_file:
+        with open(output_file, 'a', newline='') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=keys_to_extract)
             writer.writerow({key: dico_info[key] for key in keys_to_extract})
 
@@ -103,14 +102,14 @@ def parse_dico_toCSV(dico_info, keys_to_extract, output_file):
 if __name__ == '__main__':
     arguments = docopt(__doc__)
     nom_rep_input = arguments['<nom_rep_input>']
-    output = arguments['<output.txt>']
+    output_csv = arguments['<output_csv>']
 
     with open('keys_to_extract.json', 'r') as f:
         keys_to_extract = json.load(f)['keys_to_extract']
 
-    txt_liste = list_text_files(nom_rep_input)
-    dico_info = extract_info_from_txt_list(txt_liste)
-    parse_dico_toCSV(dico_info, keys_to_extract, output)
+    txt_liste = list_text_files(nom_rep_input) 
+    dico_info = extract_info_from_txt(txt_liste , keys_to_extract)
+    
+    parse_listOFdico_to_csv(dico_info, output_csv)
 
-    # bug == parcours des fichiers txt et ecrase les info -> ecrire a la voler ou faire une meta structure
-    # ajouter l'entete du fichier csv
+
---
+++ curve_rarf/readme.txt
@@ -0,0 +1,1 @@
+python extract_DSK-stdout_info.py ../../../MISTIC/Salade_I/3_curve/dsk_txt/ hifi_run1_h5info.txt
