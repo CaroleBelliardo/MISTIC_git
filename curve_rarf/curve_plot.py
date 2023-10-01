a55ab8c88d8d4d1dff176d866a486c4478fb0142
Carole Belliardo <belliardo.carole@hotmail.fr>
2023-10-01 23:50:45 +0200

    maj

--- curve_rarf/curve_plot.py
+++ curve_rarf/curve_plot.py
@@ -11,14 +11,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def import csv_to_df(csv_file):
+
+def import_csv_to_df(csv_file):
     df = pd.read_csv(csv_file, sep='\t')
     return df
 
+
 def main():
     df = import_csv_to_df(arguments['<tableau_csv>'])
-    df['techno', 'replicat', 'sampling_size'] = df['bank_uri'].str.split('_', expand=True)
+    df[['techno', 'replicat', 'sampling_size']
+       ] = df['bank_uri'].str.split('_', expand=True)
     print(df)
+
 
 if __name__ == '__main__':
     arguments = docopt.docopt(__doc__)
