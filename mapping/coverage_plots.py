2fbe7d0cec3f287fc6c2097f0a49147579ba60c5
Carole Belliardo <belliardo.carole@hotmail.fr>
2023-10-04 14:31:26 +0200

    coverage_plot

--- mapping/coverage_plots.py
+++ mapping/coverage_plots.py
@@ -4,9 +4,13 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
-import samtools_func as sf
-import bedtools_func as bf
+
+run1_reads = pd.read_csv("/kwak/hub/25_cbelliardo/MISTIC/Salade_I/mapping_SR_LR_reads/cleaned_run1__vs__hifireads.bam.sorted.coverage", sep="\t", header=None)
+run1_assembly = pd.read_csv("/kwak/hub/25_cbelliardo/MISTIC/Salade_I/mapping_SR_LR_assembly/cleaned_run1__vs__hifi_assembly.bam.sorted.coverage",  sep="\t", header=None)
 
 
+# boxplot of num reads per contig
+sns.boxplot(x=run1_reads[2])
+plt.show()
 
-run1_reads = pd.read_csv("/kwak/hub/25_cbelliardo/MISTIC/Salade_I/mapping_SR_LR_readscleaned_run1__vs__hifireads.bam.sorted.coverage", sep="\t", header=None)
+print(run1_assembly.head())
